5efe1a4a45dcdbd0a5a889ad8c73f3c73086dbb6
John Morsley <johnmorsley@gmail.com>
2020-05-10 11:08:12 +0100

    First simple implementation

---
+++ inputs.tf
@@ -0,0 +1,46 @@
+﻿#      _____                   _       
+#     |_   _|                 | |      
+#       | |  _ __  _ __  _   _| |_ ___
+#       | | | '_ \| '_ \| | | | __/ __|
+#      _| |_| | | | |_) | |_| | |_\__ \
+#     |_____|_| |_| .__/ \__,_|\__|___/
+#             | |                  
+#             |_|                  
+
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ingress" {
+  type = list(
+    object({
+      description = string
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    })
+  )
+  default = []
+}
+
+variable "egress" {
+  type = list(
+    object({
+      description = string
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    })
+  )
+  default = []
+}
---
+++ main.tf
@@ -0,0 +1,57 @@
+﻿#       _____                      _ _         
+#      / ____|                    (_) |        
+#     | (___   ___  ___ _   _ _ __ _| |_ _   _ 
+#      \___ \ / _ \/ __| | | | '__| | __| | | |
+#      ____) |  __/ (__| |_| | |  | | |_| |_| |
+#     |_____/ \___|\___|\__,_|_|  |_|\__|\__, |
+#            _____                        __/ |
+#           / ____|                      |___/ 
+#          | |  __ _ __ ___  _   _ _ __  
+#          | | |_ | '__/ _ \| | | | '_ \ 
+#          | |__| | | | (_) | |_| | |_) |
+#           \_____|_|  \___/ \__,_| .__/ 
+#                                 | |    
+#                                 |_|    
+
+resource "aws_security_group" "allow_tls" {
+  
+  name        = local.name
+  description = var.description
+  
+  vpc_id      = var.vpc_id
+
+  # Inbound...
+  dynamic "ingress" {
+
+    for_each = var.ingress
+
+    content {
+      description = ingress.value["description"]
+      from_port   = ingress.value["from_port"]
+      to_port     = ingress.value["to_port"]
+      protocol    = ingress.value["protocol"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+    }
+
+  }
+  
+  # Outbound...
+  dynamic "egress" {
+
+    for_each = var.egress
+
+    content {
+      description = egress.value["description"]
+      from_port   = egress.value["from_port"]
+      to_port     = egress.value["to_port"]
+      protocol    = egress.value["protocol"]
+      cidr_blocks = egress.value["cidr_blocks"]
+    }
+
+  }
+
+  #tags = {
+  #  Name = "allow_tls"
+  #}
+  
+}
